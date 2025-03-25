9a9fda90e82c5f4ed7cea2caf1c9a0963ecd26f4
Rikellme2475 <kamashito62@gmail.com>
2025-03-24 22:53:07 -0300

    nomes e idade podem ser digitados

--- main.java
+++ main.java
@@ -4,8 +4,13 @@ public class main {
     public static void main(String[] args){
        Scanner scan = new Scanner(System.in);
        System.out.println("digite seu nome");
-        String nome = scan.nextLine();
-        int idade = 23;
+       String nome = scan.nextLine();
+       System.out.println("digite sua idade");
+       String idadeStr = scan.nextLine();
+       int idade = Integer.parseInt(idadeStr);
+    //    System.out.println("numero: " + idade);
         System.out.printf(" seu nome é %s e você tem %d anos ", nome, idade);
+  
     }
 }
+ 
